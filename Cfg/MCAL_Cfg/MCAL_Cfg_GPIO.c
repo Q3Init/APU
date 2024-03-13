6cd44f3ec904d9c526ced8ebdf94f53d2f27895d
bayuxuan <1084797524@qq.com>
2024-03-14 00:18:08 +0800

    [MCAL][GPIO]:增加部分引脚[MCAL_UART]:删除PA2，PA3初始化

--- Cfg/MCAL_Cfg/MCAL_Cfg_GPIO.c
+++ Cfg/MCAL_Cfg/MCAL_Cfg_GPIO.c
@@ -1,26 +1,66 @@
 #include "MCAL_Cfg_GPIO.h"
 
-const GPIOSignalsCfg gpioSignalsCfgTable[GPIO_SIGNALS_CNT] = 
-{
-    {/* GPIOA PIN0 : ADC */
-        .GPIO = GPIOA,
-        .APB1_Clock = STD_OFF,
-        .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
-        {
-            .mode = GPIO_MODE_ANALOG,
-            .pin = GPIO_PIN_0,
-            .speed = GPIO_SPEED_50MHz
-        }
-    },
-    {/* GPIOA PIN0 : I/O */
-        .GPIO = GPIOA,
-        .APB1_Clock = STD_OFF,
-        .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
-        {
-            .mode = GPIO_MODE_OUT_PP,
-            .pin = GPIO_PIN_1,
-            .speed = GPIO_SPEED_50MHz
-        }
-    }
-};
-
+const GPIOSignalsCfg gpioSignalsCfgTable[GPIO_SIGNALS_CNT] =
+    {
+        {/* GPIOA PIN0 : ADC */
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+          .mode = GPIO_MODE_ANALOG,
+          .pin = GPIO_PIN_0,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+        {/* GPIOA PIN1 : I/O */
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+          .mode = GPIO_MODE_OUT_PP,
+          .pin = GPIO_PIN_1,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+        {/* GPIOA PIN2 : USART2 Tx as alternate function push-pull*/
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+          .mode = GPIO_MODE_AF_PP,
+          .pin = GPIO_PIN_2,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+        {/* GPIOA PIN3 : USART2 Rx as input floating*/
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+    
+          .mode = GPIO_MODE_IN_FLOATING,
+          .pin = GPIO_PIN_3,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+        {/* GPIOA PIN4 : I/O */
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+          .mode = GPIO_MODE_OUT_PP,
+          .pin = GPIO_PIN_4,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+        {/* GPIOA PIN5 : I/O */
+         .GPIO = GPIOA,
+         .APB1_Clock = STD_OFF,
+         .APB2_Clock = RCM_APB2_PERIPH_GPIOA,
+         {
+          .mode = GPIO_MODE_OUT_PP,
+          .pin = GPIO_PIN_5,
+          .speed = GPIO_SPEED_50MHz
+          }
+        },
+    };
--- MCAL/MCAL_UART.c
+++ MCAL/MCAL_UART.c
@@ -29,15 +29,7 @@ void MCAL_UART_Init(void)
     GPIO_configStruct.pin = GPIO_PIN_10;
     GPIO_Config(GPIOA, &GPIO_configStruct);
 
-    /* Configure USART2 Tx as alternate function push-pull */
-    GPIO_configStruct.mode = GPIO_MODE_AF_PP;
-    GPIO_configStruct.pin = GPIO_PIN_2;
-    GPIO_Config(GPIOA, &GPIO_configStruct);
 
-    /* Configure USART2 Rx as input floating */
-    GPIO_configStruct.mode = GPIO_MODE_IN_FLOATING;
-    GPIO_configStruct.pin = GPIO_PIN_3;
-    GPIO_Config(GPIOA, &GPIO_configStruct);
 
     for (uartIndex = 0;uartIndex < UART_SIGNALS_CNT;uartIndex++) {
         /* USART configuration */
