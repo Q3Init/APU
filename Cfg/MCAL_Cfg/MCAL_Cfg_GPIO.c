dccd53ee9c4cdd5f2a2111bed213cf41abec6048
bayuxuan <1084797524@qq.com>
2024-03-18 11:43:44 +0800

    [MCAL_cfg_GPIO]：修改引脚初始配置

--- Cfg/MCAL_Cfg/MCAL_Cfg_GPIO.c
+++ Cfg/MCAL_Cfg/MCAL_Cfg_GPIO.c
@@ -317,7 +317,7 @@ const GPIOSignalsCfg gpioSignalsCfgTable[GPIO_SIGNALS_CNT] =
         },
         {/* GPIOB PIN_12 : CS */
          .GPIO = GPIOB,
-         .GPIO_level = STD_LOW,
+         .GPIO_level = STD_HIGH,
          .APB1_Clock = STD_OFF,
          .APB2_Clock = RCM_APB2_PERIPH_GPIOB,
          {
