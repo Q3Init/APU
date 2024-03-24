c386e74e5970ed2123a37bbb30fc97340c3ef6da
huangrujin <163310519+huangrujin@users.noreply.github.com>
2024-03-24 22:38:05 +0800

    [BSW_Dio]更改了对GPIO引脚组别的宏封装，以及Ext_KEY和Ext_LED的引用BSW_Dio宏封装的组别的更改

--- BSW/BSW_Dio.h
+++ BSW/BSW_Dio.h
@@ -5,76 +5,176 @@
 #include "MCAL_APM32.h"
 
 /*GPIOA*/
-#define DIO_PORT_A     GPIOA
-#define DIO_PIN_IA     GPIO_PIN_4
-#define DIO_PIN_IB     GPIO_PIN_5
-#define DIO_PIN_IC     GPIO_PIN_6
-#define DIO_PIN_Iout   GPIO_PIN_7
-#define DIO_PIN_DTXD   GPIO_PIN_9
-#define DIO_PIN_DRXD   GPIO_PIN_10
+#define DIO_PORT_IA     GPIOA
+#define DIO_PIN_IA      GPIO_PIN_4
+
+#define DIO_PORT_IB     GPIOA
+#define DIO_PIN_IB      GPIO_PIN_5
+
+#define DIO_PORT_IC     GPIOA
+#define DIO_PIN_IC      GPIO_PIN_6
+
+#define DIO_PORT_Iout   GPIOA
+#define DIO_PIN_Iout    GPIO_PIN_7
+
+#define DIO_PORT_DTXD   GPIOA
+#define DIO_PIN_DTXD    GPIO_PIN_9
+
+#define DIO_PORT_DRXD   GPIOA
+#define DIO_PIN_DRXD    GPIO_PIN_10
+
+#define DIO_PORT_DirControl  GPIOA
 #define DIO_PIN_DirControl   GPIO_PIN_11
 
 
 /*GPIOB*/
-#define DIO_PORT_B     GPIOB
-#define DIO_PIN_Uc_m   GPIO_PIN_0
-#define DIO_PIN_Uout_m GPIO_PIN_1
+#define DIO_PORT_Uc_m    GPIOB
+#define DIO_PIN_Uc_m     GPIO_PIN_0
+
+#define DIO_PORT_Uout_m  GPIOB
+#define DIO_PIN_Uout_m   GPIO_PIN_1
+
+#define DIO_PORT_Power_led GPIOB
 #define DIO_PIN_Power_led  GPIO_PIN_2
-#define DIO_PIN_D01_C  GPIO_PIN_5
-#define DIO_PIN_D02_C  GPIO_PIN_6
-#define DIO_PIN_D03_C  GPIO_PIN_7
-#define DIO_PIN_D04_C  GPIO_PIN_8
-#define DIO_PIN_D05_C  GPIO_PIN_9
-#define DIO_PIN_D06_C  GPIO_PIN_10
-#define DIO_PIN_D07_C  GPIO_PIN_11
-#define DIO_PIN_RS4CS  GPIO_PIN_12
-#define DIO_PIN_SCK    GPIO_PIN_13
-#define DIO_PIN_MISO   GPIO_PIN_14
-#define DIO_PIN_MOSI   GPIO_PIN_15
+
+#define DIO_PORT_D01_C   GPIOB
+#define DIO_PIN_D01_C    GPIO_PIN_5
+
+#define DIO_PORT_D02_C   GPIOB
+#define DIO_PIN_D02_C    GPIO_PIN_6
+
+#define DIO_PORT_D03_C   GPIOB
+#define DIO_PIN_D03_C    GPIO_PIN_7
+
+#define DIO_PORT_D04_C   GPIOB
+#define DIO_PIN_D04_C    GPIO_PIN_8
+
+#define DIO_PORT_D05_C   GPIOB
+#define DIO_PIN_D05_C    GPIO_PIN_9
+
+#define DIO_PORT_D06_C   GPIOB
+#define DIO_PIN_D06_C    GPIO_PIN_10
+
+#define DIO_PORT_D07_C   GPIOB
+#define DIO_PIN_D07_C    GPIO_PIN_11
+
+#define DIO_PORT_RS4CS   GPIOB
+#define DIO_PIN_RS4CS    GPIO_PIN_12
+
+#define DIO_PORT_SCK     GPIOB
+#define DIO_PIN_SCK      GPIO_PIN_13
+
+#define DIO_PORT_MISO    GPIOB
+#define DIO_PIN_MISO     GPIO_PIN_14
+
+#define DIO_PORT_MOSI    GPIOB
+#define DIO_PIN_MOSI     GPIO_PIN_15
 
 /*GPIOC*/
-#define DIO_PORT_C     GPIOC
-#define DIO_PIN_Ua_m   GPIO_PIN_3
-#define DIO_PIN_Ub_m   GPIO_PIN_4
-#define DIO_PIN_cancel GPIO_PIN_5
-#define DIO_PIN_DI1_m  GPIO_PIN_11
-#define DIO_PIN_DI2_m  GPIO_PIN_12
-#define DIO_PIN_LCD_Power GPIO_PIN_13
-#define DIO_PIN_OSC32_IN  GPIO_PIN_14
-#define DIO_PIN_OSC32_OUT GPIO_PIN_15
+#define DIO_PORT_Ua_m    GPIOC
+#define DIO_PIN_Ua_m     GPIO_PIN_3
+
+#define DIO_PORT_Ub_m    GPIOC
+#define DIO_PIN_Ub_m     GPIO_PIN_4
+
+#define DIO_PORT_cancel  GPIOC
+#define DIO_PIN_cancel   GPIO_PIN_5
+
+#define DIO_PORT_DI1_m   GPIOC
+#define DIO_PIN_DI1_m    GPIO_PIN_11
+
+#define DIO_PORT_DI2_m   GPIOC
+#define DIO_PIN_DI2_m    GPIO_PIN_12
+
+#define DIO_PORT_LCD_Power GPIOC
+#define DIO_PIN_LCD_Power  GPIO_PIN_13
+
+#define DIO_PORT_OSC32_IN  GPIOC
+#define DIO_PIN_OSC32_IN   GPIO_PIN_14
+
+#define DIO_PORT_OSC32_OUT GPIOC
+#define DIO_PIN_OSC32_OUT  GPIO_PIN_15
 
 
 /*GPIOD*/
-#define DIO_PORT_D     GPIOD
-#define DIO_PIN_DI3_m  GPIO_PIN_0
-#define DIO_PIN_DI4_m  GPIO_PIN_1
-#define DIO_PIN_DI5_m  GPIO_PIN_2
-#define DIO_PIN_DI6_m  GPIO_PIN_3
-#define DIO_PIN_DI7_m  GPIO_PIN_4
-#define DIO_PIN_DI8_m  GPIO_PIN_5
-#define DIO_PIN_DI9_m  GPIO_PIN_6
-#define DIO_PIN_reset GPIO_PIN_7
-#define DIO_PIN_return GPIO_PIN_8
-#define DIO_PIN_enter  GPIO_PIN_9
-#define DIO_PIN_up     GPIO_PIN_10
-#define DIO_PIN_down   GPIO_PIN_11
-#define DIO_PIN_left   GPIO_PIN_12
-#define DIO_PIN_right  GPIO_PIN_13
+#define DIO_PORT_DI3_m   GPIOD
+#define DIO_PIN_DI3_m    GPIO_PIN_0
+
+#define DIO_PORT_DI4_m   GPIOD
+#define DIO_PIN_DI4_m    GPIO_PIN_1
+
+#define DIO_PORT_DI5_m   GPIOD
+#define DIO_PIN_DI5_m    GPIO_PIN_2
+
+#define DIO_PORT_DI6_m   GPIOD
+#define DIO_PIN_DI6_m    GPIO_PIN_3
+
+#define DIO_PORT_DI7_m   GPIOD
+#define DIO_PIN_DI7_m    GPIO_PIN_4
+
+#define DIO_PORT_DI8_m   GPIOD
+#define DIO_PIN_DI8_m    GPIO_PIN_5
+
+#define DIO_PORT_DI9_m   GPIOD
+#define DIO_PIN_DI9_m    GPIO_PIN_6
+
+#define DIO_PORT_reset   GPIOD
+#define DIO_PIN_reset    GPIO_PIN_7
+
+#define DIO_PORT_return  GPIOD
+#define DIO_PIN_return   GPIO_PIN_8
+
+#define DIO_PORT_enter   GPIOD
+#define DIO_PIN_enter    GPIO_PIN_9
+
+#define DIO_PORT_up      GPIOD
+#define DIO_PIN_up       GPIO_PIN_10
+
+#define DIO_PORT_down    GPIOD
+#define DIO_PIN_down     GPIO_PIN_11
+
+#define DIO_PORT_left    GPIOD
+#define DIO_PIN_left     GPIO_PIN_12
+
+#define DIO_PORT_right   GPIOD
+#define DIO_PIN_right    GPIO_PIN_13
+
+#define DIO_PORT_subtract GPIOD
 #define DIO_PIN_subtract  GPIO_PIN_14
-#define DIO_PIN_plus   GPIO_PIN_15
+
+#define DIO_PORT_plus    GPIOD
+#define DIO_PIN_plus     GPIO_PIN_15
 
 /*GPIOE*/
-#define DIO_PORT_E     GPIOE
-#define DIO_PIN_D08_C  GPIO_PIN_0
-#define DIO_PIN_working GPIO_PIN_7
-#define DIO_PIN_keyopen GPIO_PIN_8
+#define DIO_PORT_D08_C   GPIOE
+#define DIO_PIN_D08_C    GPIO_PIN_0
+
+#define DIO_PORT_working GPIOE
+#define DIO_PIN_working  GPIO_PIN_7
+
+#define DIO_PORT_keyopen GPIOE
+#define DIO_PIN_keyopen  GPIO_PIN_8
+
+#define DIO_PORT_keyclose  GPIOE
 #define DIO_PIN_keyclose   GPIO_PIN_9
-#define DIO_PIN_fault  GPIO_PIN_10
-#define DIO_PIN_warning  GPIO_PIN_11
+
+#define DIO_PORT_fault   GPIOE
+#define DIO_PIN_fault    GPIO_PIN_10
+
+#define DIO_PORT_warning   GPIOE
+#define DIO_PIN_warning    GPIO_PIN_11
+
+#define DIO_PORT_communication  GPIOE
 #define DIO_PIN_communication   GPIO_PIN_12
-#define DIO_PIN_EI13_m  GPIO_PIN_13
-#define DIO_PIN_HC_C   GPIO_PIN_14
-#define DIO_PIN_TQ_C   GPIO_PIN_15
+
+#define DIO_PORT_EI13_m  GPIOE
+#define DIO_PIN_EI13_m   GPIO_PIN_13
+
+#define DIO_PORT_HC_C    GPIOE
+#define DIO_PIN_HC_C     GPIO_PIN_14
+
+#define DIO_PORT_TQ_C    GPIOE
+#define DIO_PIN_TQ_C     GPIO_PIN_15
 
 extern uint8 BSW_Dio_ReadInputBit(GPIO_T* port, uint16_t pin);
 extern void BSW_Dio_WriteBitValue(GPIO_T* port, uint16_t pin, uint8_t bitVal);
--- ext-driver/Ext_KEY.c
+++ ext-driver/Ext_KEY.c
@@ -3,10 +3,10 @@
 uint8 KEY_Reset(void)
 {
     uint8 keynum=10;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_reset)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_reset)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
         keynum=KEY_RESET;
     }
     return keynum;
@@ -15,10 +15,10 @@ uint8 KEY_Reset(void)
 uint8 KEY_Return(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_return)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_return,DIO_PIN_return)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_return)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_return,DIO_PIN_return)==1)
         keynum=KEY_RETURN;
     }
     return keynum;
@@ -27,10 +27,10 @@ uint8 KEY_Return(void)
 uint8 KEY_Enter(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_enter)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_enter,DIO_PIN_enter)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_enter)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_enter,DIO_PIN_enter)==1)
         keynum=KEY_ENTER;
     }
     return keynum;
@@ -39,10 +39,10 @@ uint8 KEY_Enter(void)
 uint8 KEY_Up(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_up)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_up,DIO_PIN_up)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_up)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_up,DIO_PIN_up)==1)
         keynum=KEY_UP;
     }
     return keynum;
@@ -51,10 +51,10 @@ uint8 KEY_Up(void)
 uint8 KEY_Down(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_down)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_down,DIO_PIN_down)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_down)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_down,DIO_PIN_down)==1)
         keynum=KEY_DOWN;
     }
     return keynum;
@@ -63,10 +63,10 @@ uint8 KEY_Down(void)
 uint8 KEY_Left(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_left)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_left,DIO_PIN_left)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_left)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_left,DIO_PIN_left)==1)
         keynum=KEY_LEFT;
     }
     return keynum;
@@ -75,10 +75,10 @@ uint8 KEY_Left(void)
 uint8 KEY_Right(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_right)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_right,DIO_PIN_right)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_right)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_right,DIO_PIN_right)==1)
         keynum=KEY_RIGHT;
     }
     return keynum;
@@ -87,10 +87,10 @@ uint8 KEY_Right(void)
 uint8 KEY_Minus(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_subtract)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_right,DIO_PIN_subtract)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_subtract)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_right,DIO_PIN_subtract)==1)
         keynum=KEY_MINUS;
     }
     return keynum;
@@ -99,10 +99,10 @@ uint8 KEY_Minus(void)
 uint8 KEY_Plus(void)
 {
     uint8 keynum=0;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_plus)==1)
+    if(BSW_Dio_ReadInputBit(DIO_PORT_plus,DIO_PIN_plus)==1)
     {
         OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_D,DIO_PIN_plus)==1)
+        while(BSW_Dio_ReadInputBit(DIO_PORT_plus,DIO_PIN_plus)==1)
         keynum=KEY_PLUS;
     }
     return keynum;
