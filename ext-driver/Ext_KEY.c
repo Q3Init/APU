740af2313c9295bcc35e1191c2b8e0fa53e42d9d
Loki <2914927818@qq.com>
2024-04-14 15:11:15 +0800

    [EXT-DRIVER] :1、reset按键有吗？ 2、添加源文件

--- ext-driver/Ext_KEY.c
+++ ext-driver/Ext_KEY.c
@@ -3,12 +3,12 @@
 uint8 KEY_Reset(void)
 {
     uint8 keynum=10;
-    if(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
-    {
-        OS_Delay_us(1000);
-        while(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
-        keynum=KEY_RESET;
-    }
+    // if(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
+    // {
+    //     OS_Delay_us(1000);
+    //     while(BSW_Dio_ReadInputBit(DIO_PORT_reset,DIO_PIN_reset)==1)
+    //     keynum=KEY_RESET;
+    // }
     return keynum;
 }
 
