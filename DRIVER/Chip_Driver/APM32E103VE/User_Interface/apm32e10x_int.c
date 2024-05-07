d7f893786261bbe04a945c8c78908b6074b0aea7
Q3Init <2914927818@qq.com>
2024-05-07 21:36:11 +0800

    merge develop OS

--- DRIVER/Chip_Driver/APM32E103VE/User_Interface/apm32e10x_int.c
+++ DRIVER/Chip_Driver/APM32E103VE/User_Interface/apm32e10x_int.c
@@ -122,9 +122,11 @@ void UsageFault_Handler(void)
  * @retval  None
  *
  */
+#ifdef EASY_OS
 void SVC_Handler(void)
 {
 }
+#endif
 
 /*!
  * @brief   This function handles Debug Monitor exception
@@ -146,11 +148,11 @@ void DebugMon_Handler(void)
  * @retval  None
  *
  */
-
+#ifdef EASY_OS
 void PendSV_Handler(void)
 {
 }
-
+#endif
 
 /**@} end of group GPIO_Toggle_INT_Functions */
 /**@} end of group GPIO_Toggle */
