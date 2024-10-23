2bf1157354746634bc8ecb04ef5acd04e18013ca
“JackSahar” <1587015694@qq.com>
2024-10-23 23:12:13 +0800

    [OS]修改各线程的优先级并删除未使用的BSW_Adcif_Mainfunction线程

--- APP/FreeRTOS_TASK.c
+++ APP/FreeRTOS_TASK.c
@@ -13,7 +13,7 @@
 
  /* 创建任务句柄 */
 TaskHandle_t AppTaskCreate_Handle = NULL;
-static TaskHandle_t BSW_Adcif_Mainfunction_Task_Handle = NULL;
+//static TaskHandle_t BSW_Adcif_Mainfunction_Task_Handle = NULL;
 static TaskHandle_t menu_kernel_schedule_Task_Handle = NULL;
 static TaskHandle_t APP_test_Mainfunction_Task_Handle = NULL;
 static TaskHandle_t APP_Protection_Mnt_Task_Handle = NULL;
@@ -26,23 +26,23 @@ void AppTaskCreate(void)
   
   taskENTER_CRITICAL();
   
-  /* BSW_Adcif_Mainfunction */
-  xReturn = xTaskCreate((TaskFunction_t )BSW_Adcif_Mainfunction,
-                        (const char*    )"BSW_Adcif_Task",
-                        (uint16_t       )512,
-                        (void*          )NULL,
-                        (UBaseType_t    )2,
-                        (TaskHandle_t*  )&BSW_Adcif_Mainfunction_Task_Handle);
-  if(pdPASS == xReturn) {
-    Log_d("BSW_Adcif_Mainfunction task create OK!\r\n");
-  }
+  // /* BSW_Adcif_Mainfunction */
+  // xReturn = xTaskCreate((TaskFunction_t )BSW_Adcif_Mainfunction,
+  //                       (const char*    )"BSW_Adcif_Task",
+  //                       (uint16_t       )512,
+  //                       (void*          )NULL,
+  //                       (UBaseType_t    )2,
+  //                       (TaskHandle_t*  )&BSW_Adcif_Mainfunction_Task_Handle);
+  // if(pdPASS == xReturn) {
+  //   Log_d("BSW_Adcif_Mainfunction task create OK!\r\n");
+  // }
 
   /* menu_kernel_schedule */
   xReturn = xTaskCreate((TaskFunction_t )menu_kernel_schedule,
                         (const char*    )"menu_kernel_schedule_Task",
                         (uint16_t       )2048,
                         (void*          )NULL,
-                        (UBaseType_t    )3,
+                        (UBaseType_t    )4,
                         (TaskHandle_t*  )&menu_kernel_schedule_Task_Handle);
   if(pdPASS == xReturn) {
     Log_d("menu_kernel_schedule task create OK!\r\n");
@@ -53,7 +53,7 @@ void AppTaskCreate(void)
                         (const char*    )"APP_test_Task",
                         (uint16_t       )256,
                         (void*          )NULL,
-                        (UBaseType_t    )4,
+                        (UBaseType_t    )2,
                         (TaskHandle_t*  )&APP_test_Mainfunction_Task_Handle);
   if(pdPASS == xReturn) {
     Log_d("APP_test_Mainfunction task create OK!\r\n");
@@ -64,7 +64,7 @@ void AppTaskCreate(void)
                         (const char*    )"Protection_Mnt_Task",
                         (uint16_t       )1024,
                         (void*          )NULL,
-                        (UBaseType_t    )5,
+                        (UBaseType_t    )4,
                         (TaskHandle_t*  )&APP_Protection_Mnt_Task_Handle);
   if(pdPASS == xReturn) {
     Log_d("APP_Protection_Management_Loop task create OK!\r\n");
@@ -75,7 +75,7 @@ void AppTaskCreate(void)
                         (const char*    )"Protection_Backend_Task",
                         (uint16_t       )1024,
                         (void*          )NULL,
-                        (UBaseType_t    )5,
+                        (UBaseType_t    )4,
                         (TaskHandle_t*  )&APP_Protection_Backend_Task_Handle);
   if(pdPASS == xReturn) {
     Log_d("APP_Protection_Backend_Loop task create OK!\r\n");
@@ -86,7 +86,7 @@ void AppTaskCreate(void)
                         (const char*    )"App_fault_management_Task",
                         (uint16_t       )1536,
                         (void*          )NULL,
-                        (UBaseType_t    )6,
+                        (UBaseType_t    )3,
                         (TaskHandle_t*  )&APP_fault_management_Task_Handle);
   if(pdPASS == xReturn) {
     Log_d("App_fault_management task create OK!\r\n");
@@ -96,7 +96,7 @@ void AppTaskCreate(void)
                            (const char*)"Led_Task",
                            (uint16_t)64,
                            (void*)NULL,
-                           (UBaseType_t)7,
+                           (UBaseType_t)1,
                            (TaskHandle_t*)NULL );
 
     if ( pdPASS == xReturn )
@@ -109,7 +109,7 @@ void AppTaskCreate(void)
                            (const char*)"BSW_ModbusMainfunction",
                            (uint16_t)512,
                            (void*)NULL,
-                           (UBaseType_t)8,
+                           (UBaseType_t)2,
                            (TaskHandle_t*)NULL );
 
     if ( pdPASS == xReturn )
